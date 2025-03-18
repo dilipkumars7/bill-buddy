f21cce30ff64d649a22c10365c83397c23bd3e6e
DILIP KUMAR S <115959093+dilipkumars7@users.noreply.github.com>
2025-03-18 16:05:19 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,32 @@
+"use client";
 import React from 'react'
 import './common.css'
+import SdkAvatar from './avatar';
+import localFont from "next/font/local";
+
+const SKDsFontAp = localFont({ 
+  src: '../../public/fonts/apercu_regular_pro.otf',
+  fallback:['sans-serif' ]
+})
+
 
 export default function SdkNavbar() {
   return (
-    <div className='h-[56px] z-10 fixed w-full top-0'>
+    <div className={`${SKDsFontAp.className} tracking-wide h-[56px] z-10 fixed w-full top-0`}>
       <header className='nav-header relative flex'>
-        Nav
+        <div className='w-full flex gap-5'>
+          <div className='flex items-center gap-4'>
+            <span className='text-lg font-bold'>Logo</span>
+          </div>
+          <div className='flex items-center gap-4'>
+            <button className='font-bold'>Home</button>
+            <button className='font-bold'>About</button>
+            <button className='font-bold'>Create</button>
+          </div>
+        </div>
+        <div className='flex items-center h-full gap-4'>
+          <SdkAvatar size={34} email="Dilip.Sivam@gmail.com" />
+        </div>
       </header>
     </div>
   )
